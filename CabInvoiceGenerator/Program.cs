f7be38fb9f9d81ccdfa35b895f071bd60416463f
anas salagar <alisalagarf@gmail.com>
2021-11-21 21:55:37 +0530

    [anas added for calculate fare of journey and test for checking]

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" WELCOME TO CAB INVOICE GENERATOR");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double toatalFare = invoiceGenerator.CaculateFare(2.0, 5);
+            Console.WriteLine("Total Fare Of Journey : " + toatalFare);
+        }
+    }
+}
